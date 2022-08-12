281151c96fb73eba973ce0f1774708750be6736a
Deisy <test@test.com>
2022-08-12 12:49:56 -0400

    added

--- routes/web.php
+++ routes/web.php
@@ -1,12 +1,17 @@
 <?php
 
 use App\Models\Project;
+use App\Models\Workexperience;
 use App\Http\Controllers\ConsoleController;
 use App\Http\Controllers\ProjectsController;
 use App\Http\Controllers\TypesController;
 use App\Http\Controllers\UsersController;
-use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\SkillsController;
+use App\Http\Controllers\EducationsController;
+use App\Http\Controllers\ContentBlogsController;
+use App\Http\Controllers\WorkexperiencesController;
 
+use Illuminate\Support\Facades\Route;
 /*
 |--------------------------------------------------------------------------
 | Web Routes    
@@ -20,15 +25,16 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome', [
-        'projects' => Project::all()
+        'projects' => Project::all(),
+        'experiences' => Workexperience::all(),
     ]);
 });
 
-Route::get('/project/{project:slug}', function (Project $project) {
-    return view('project', [
-        'project' => $project
-    ]);
-})->where('project', '[A-z\-]+');
+// Route::get('/project/{project:slug}', function (Project $project) {
+//     return view('project', [
+//         'project' => $project
+//     ]);
+// })->where('project', '[A-z\-]+');
 
 Route::get('/console/logout', [ConsoleController::class, 'logout'])->middleware('auth');
 Route::get('/console/login', [ConsoleController::class, 'loginForm'])->middleware('guest');
@@ -51,9 +57,45 @@ Route::get('/console/users/edit/{user:id}', [UsersController::class, 'editForm']
 Route::post('/console/users/edit/{user:id}', [UsersController::class, 'edit'])->where('user', '[0-9]+')->middleware('auth');
 Route::get('/console/users/delete/{user:id}', [UsersController::class, 'delete'])->where('user', '[0-9]+')->middleware('auth');
 
+//type
 Route::get('/console/types/list', [TypesController::class, 'list'])->middleware('auth');
 Route::get('/console/types/add', [TypesController::class, 'addForm'])->middleware('auth');
 Route::post('/console/types/add', [TypesController::class, 'add'])->middleware('auth');
 Route::get('/console/types/edit/{type:id}', [TypesController::class, 'editForm'])->where('type', '[0-9]+')->middleware('auth');
 Route::post('/console/types/edit/{type:id}', [TypesController::class, 'edit'])->where('type', '[0-9]+')->middleware('auth');
 Route::get('/console/types/delete/{type:id}', [TypesController::class, 'delete'])->where('type', '[0-9]+')->middleware('auth');
+
+//skills
+Route::get('/console/skills/list', [SkillsController::class, 'list'])->middleware('auth');
+Route::get('/console/skills/add', [SkillsController::class, 'addForm'])->middleware('auth');
+Route::post('/console/skills/add', [SkillsController::class, 'add'])->middleware('auth');
+Route::get('/console/skills/edit/{skill:id}', [SkillsController::class, 'editForm'])->where('skill', '[0-9]+')->middleware('auth');
+Route::post('/console/skills/edit/{skill:id}', [SkillsController::class, 'edit'])->where('skill', '[0-9]+')->middleware('auth');
+Route::get('/console/skills/delete/{skill:id}', [SkillsController::class, 'delete'])->where('skill', '[0-9]+')->middleware('auth');
+Route::get('/console/skills/image/{skill:id}', [SkillsController::class, 'imageForm'])->where('skill', '[0-9]+')->middleware('auth');
+Route::post('/console/skills/image/{skill:id}', [SkillsController::class, 'image'])->where('skill', '[0-9]+')->middleware('auth');
+
+//education
+Route::get('/console/educations/list', [EducationsController::class, 'list'])->middleware('auth');
+Route::get('/console/educations/add', [EducationsController::class, 'addForm'])->middleware('auth');
+Route::post('/console/educations/add', [EducationsController::class, 'add'])->middleware('auth');
+Route::get('/console/educations/edit/{education:id}', [EducationsController::class, 'editForm'])->where('education', '[0-9]+')->middleware('auth');
+Route::post('/console/educations/edit/{education:id}', [EducationsController::class, 'edit'])->where('education', '[0-9]+')->middleware('auth');
+Route::get('/console/educations/delete/{education:id}', [EducationsController::class, 'delete'])->where('education', '[0-9]+')->middleware('auth');
+
+//contentblogs
+Route::get('/console/contentblogs/list', [ContentBlogsController::class, 'list'])->middleware('auth');
+Route::get('/console/contentblogs/add', [ContentBlogsController::class, 'addForm'])->middleware('auth');
+Route::post('/console/contentblogs/add', [ContentBlogsController::class, 'add'])->middleware('auth');
+Route::get('/console/contentblogs/edit/{contentblog:id}', [ContentBlogsController::class, 'editForm'])->where('education', '[0-9]+')->middleware('auth');
+Route::post('/console/contentblogs/edit/{contentblog:id}', [ContentBlogsController::class, 'edit'])->where('education', '[0-9]+')->middleware('auth');
+Route::get('/console/contentblogs/delete/{contentblog:id}', [ContentBlogsController::class, 'delete'])->where('education', '[0-9]+')->middleware('auth');
+
+
+//contentblogs
+Route::get('/console/workexperiences/list', [WorkexperiencesController::class, 'list'])->middleware('auth');
+Route::get('/console/workexperiences/add', [WorkexperiencesController::class, 'addForm'])->middleware('auth');
+Route::post('/console/workexperiences/add', [WorkexperiencesController::class, 'add'])->middleware('auth');
+Route::get('/console/workexperiences/edit/{workexperience:id}', [WorkexperiencesController::class, 'editForm'])->where('education', '[0-9]+')->middleware('auth');
+Route::post('/console/workexperiences/edit/{workexperience:id}', [WorkexperiencesController::class, 'edit'])->where('education', '[0-9]+')->middleware('auth');
+Route::get('/console/workexperiences/delete/{workexperience:id}', [WorkexperiencesController::class, 'delete'])->where('education', '[0-9]+')->middleware('auth');
