5da63695566255ee8d24d7d46b4d99a1bf639993
Deisy <test@test.com>
2022-08-12 13:14:20 -0400

    update

--- database/migrations/2022_08_01_041409_create_projects_table.php
+++ database/migrations/2022_08_01_041409_create_projects_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateProjectsTable extends Migration
+return new class extends Migration
 {
     /**
      * Run the migrations.
@@ -17,7 +17,7 @@ class CreateProjectsTable extends Migration
             $table->id();
             $table->string('title');
             $table->string('url')->nullable();
-            $table->string('slug')->unique();
+            $table->string('slug')->nullable();
             $table->string('image')->nullable();
             $table->text('content');
             $table->foreignId('type_id');
@@ -35,4 +35,4 @@ class CreateProjectsTable extends Migration
     {
         Schema::dropIfExists('projects');
     }
-}
+};
